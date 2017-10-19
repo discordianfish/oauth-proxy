0e5c9c540c98d4e6ac32d54c1cebae0eb4e4673e
Simo Sorce <simo@redhat.com>
2017-10-19 16:20:08 -0400

    Allow adding the system roots in addition to a specified ca file
    This is to allow the openshift provider to be backwards compatible
    when it sets the default, which happens every time it does not
    have an explicit configuration. Otherwise system roots can never
    be automatically queried.
    
    Users are advise to explicitly set the openshift-ca parameter if
    they do not want to allow system roots to be trusted.
    
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- util/util.go
+++ util/util.go
@@ -4,13 +4,24 @@ import (
 	"crypto/x509"
 	"fmt"
 	"io/ioutil"
+	"log"
 )
 
-func GetCertPool(paths []string) (*x509.CertPool, error) {
+func GetCertPool(paths []string, system_roots bool) (*x509.CertPool, error) {
 	if len(paths) == 0 {
 		return nil, fmt.Errorf("Invalid empty list of Root CAs file paths")
 	}
-	pool := x509.NewCertPool()
+	var pool *x509.CertPool
+	if system_roots {
+		// ignore errors
+		pool, _ = x509.SystemCertPool()
+		if pool == nil {
+			log.Printf("No system certificates found")
+			pool = x509.NewCertPool()
+		}
+	} else {
+		pool = x509.NewCertPool()
+	}
 	for _, path := range paths {
 		data, err := ioutil.ReadFile(path)
 		if err != nil {
